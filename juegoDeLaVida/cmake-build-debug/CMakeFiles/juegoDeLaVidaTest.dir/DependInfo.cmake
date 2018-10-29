3f70e22e64294b5dac5daa4bc3c31bcd9a7e720c
Marti-lp <martina.lopez.2605@gmail.com>
2018-10-29 19:49:35 -0300

    Commit inicial, agrego archivos

---
+++ juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/main.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/main.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/solucion.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/solucion.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/tests/densidadPoblacionTEST.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/densidadPoblacionTEST.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/tests/enCrecimientoTEST.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/enCrecimientoTEST.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/tests/esPeriodicoTEST.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/esPeriodicoTEST.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/tests/esValidoTEST.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/esValidoTEST.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/tests/evolucionDePosicionTEST.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/evolucionDePosicionTEST.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/tests/evolucionMultipleTEST.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/evolucionMultipleTEST.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/tests/evolucionToroideTEST.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/evolucionToroideTEST.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/tests/fusionarTEST.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/fusionarTEST.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/tests/posicionesVivasTEST.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/posicionesVivasTEST.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/tests/primosLejanosTEST.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/primosLejanosTEST.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/tests/seleccionNaturalTEST.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/seleccionNaturalTEST.o"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/tests/vistaTrasladadaTEST.cpp" "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/CMakeFiles/juegoDeLaVidaTest.dir/tests/vistaTrasladadaTEST.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../lib/googletest-master/googletest/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/lib/googletest-master/googlemock/gtest/CMakeFiles/gtest.dir/DependInfo.cmake"
+  "/home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/lib/googletest-master/googlemock/gtest/CMakeFiles/gtest_main.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ juegoDeLaVida/cmake-build-debug/lib/googletest-master/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/lib/googletest-master/googletest
+# Build directory: /home/martina/Escritorio/Algo1/TP Implementacion/juegoDeLaVida/cmake-build-debug/lib/googletest-master/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
